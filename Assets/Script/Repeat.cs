a6c6c44f91e986ed472d598f4c3a989a584fc674
SeungHoon2 <63640466+SeungHoon2@users.noreply.github.com>
2020-10-04 13:26:25 +0900

    SH
    SH

--- Assets/Script/Repeat.cs
+++ Assets/Script/Repeat.cs
@@ -4,24 +4,27 @@ using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
 
-public class RepeatSky : MonoBehaviour
+public class Repeat : MonoBehaviour
 {
-    [SerializeField] [Range(1f, 20f)] float speed = 3f;
+    [SerializeField] [Range(1f, 20f)] float speed = -3f;
     [SerializeField] float posValue;
 
     Vector2 startPos;
     float newPos;
+  
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position;         
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         newPos = Mathf.Repeat(Time.time * speed, posValue);
-        transform.position = startPos + Vector2.right * newPos;
+        transform.position = startPos + Vector2.left * newPos;
     }
 }
