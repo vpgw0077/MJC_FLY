11d68b4ab37de0e95704279f94233edd91da40cc
SeungHoon2 <63640466+SeungHoon2@users.noreply.github.com>
2020-10-05 23:46:50 +0900

    SH
    SH

---
+++ Assets/Script/PiayerConroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiayerConroller : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    [SerializeField]
+    private float power;
+
+    public void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.velocity = Vector2.up * power;
+        }
+    }
+
+
+
+}
