1451531634f10e69f2569f2be23c2d5091ce9c53
SeungHoon2 <63640466+SeungHoon2@users.noreply.github.com>
2020-11-26 07:20:49 +0900

    Scripts for Margnet
    Scripts for Margnet

---
+++ Assets/Scripts For Magnet/Coin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//SUBSCRIBE TO VIN CODES FOR MORE FREE SCRIPTS IN FUTURE VIDEOS :)
+
+public class Coin : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    public float moveSpeed = 17f;
+
+    CoinMove coinMoveScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        coinMoveScript = gameObject.GetComponent<CoinMove>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coin Detector")
+        {
+            coinMoveScript.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts For Magnet/CoinMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//SUBSCRIBE TO VIN CODES FOR MORE FREE SCRIPTS IN FUTURE VIDEOS :)
+
+public class CoinMove : MonoBehaviour
+{
+
+    Coin coinScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinScript = gameObject.GetComponent<Coin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, coinScript.playerTransform.position,
+            coinScript.moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player Bubble")
+        {
+            //Add count or give points etc etc.
+            Destroy(gameObject);
+        }
+    }
+}
